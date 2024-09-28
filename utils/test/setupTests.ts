7babb6715951fcd474855cfcc81d308f8083e387
hsskey <slingshot800@gmail.com>
2024-09-28 22:16:53 +0900

    test: 테스트 환경구성

---
+++ utils/test/setupTests.ts
@@ -0,0 +1,24 @@
+import { afterAll, afterEach, vi } from 'vitest'
+import '@testing-library/jest-dom'
+
+afterEach(() => {
+  vi.clearAllMocks()
+})
+
+afterAll(() => {
+  vi.resetAllMocks()
+})
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(),
+    removeListener: vi.fn(),
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+})
